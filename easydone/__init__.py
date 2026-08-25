6ed00cb5a4423933f01e1bbdf34f76833ea087b7
Pedro A. Rosquete Ares <rosquetearespedro06@gmail.com>
2026-08-25 07:56:41 -0400

    Correctly setting version now.

--- easydone/__init__.py
+++ easydone/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import version, PackageNotFoundError
 
 try:
     # use the distribution name from pyproject 'project.name' (make sure they match)
-    __version__ = ("easydone-task-tracker")
+    __version__ = version("easydone-task-tracker")
 except PackageNotFoundError:
     # Package isn't installed (dev environment). Optional fallback:
     __version__ = "0.0.0+dev"
